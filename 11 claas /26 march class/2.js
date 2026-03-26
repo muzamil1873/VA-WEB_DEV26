bb0b210fa3480d8623440c7d474ca936ddde0448
muzamil1873 <miaraj1873@gmail.com>
2026-03-26 14:58:46 +0000

    husm

--- 11 claas /26 march class/2.js
+++ 11 claas /26 march class/2.js
@@ -1,6 +1,4 @@
-document.write(introduction <br>)
-let name1=muhammad;
-let name2=muzamil;
-let name3=jagirani;
-let result=name1+name2+name3
-document.write(+result)
+let firstname=muhammad;
+let lastname=muzamil;
+let fullname=firstname +''+lastname;
+document.writeln(+result)
