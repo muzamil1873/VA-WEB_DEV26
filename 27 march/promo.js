7e240efb3b692b1d967d9931fd2e6cef399bbfdd
muzamil1873 <miaraj1873@gmail.com>
2026-03-27 14:10:09 +0000

    nana

---
+++ 27 march/promo.js
@@ -0,0 +1,8 @@
+// arithamatic operators:
+// + - * / %
+let a = parseint(prompt("enter number 1"));
+let b = parseint(prompt("enter number 2"));
+document.write("Addition:" +(a+b)+"<br>");
+document.write("subtraction:" +(a-b)+"<br>");
+document.write("multiplication:" +(a*b)+"<br>");
+document.write("division:" +(a/b)+"<br>");
