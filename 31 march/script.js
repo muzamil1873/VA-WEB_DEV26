64f128c91b52f50d40046d89cace371624c0591e
muzamil1873 <miaraj1873@gmail.com>
2026-03-31 15:58:47 +0000

    JJJ

--- 31 march/script.js
+++ 31 march/script.js
@@ -1,4 +1,24 @@
-let number =prompt("enter any number");
-for(let i = 1; i <= 10; i++){
-    document.write(number + "x"  + i +"=" +(number*i) + "<br>")
+// let number =prompt("enter any number");
+// for(let i = 1; i <= 10; i++){
+//     document.write(number + "x"  + i +"=" +(number*i) + "<br>")
+// }
+var maths=parseInt(prompt("enter your marks"));
+var english=parseInt(prompt("enter your marks"));
+var science=parseInt(prompt("enter your marks"));
+var urdu=parseInt(prompt("enter your marks"));
+var sindhi=parseInt(prompt("enter your marks"));
+document.write("your marks in maths"+maths +"<br>")
+document.write("your marks in english"+english +"<br>")
+document.write("your marks in science"+science +"<br>")
+document.write("your marks in urdu"+urdu +"<br>")
+document.write("your marks in sindhi"+sindhi+"<br>")
+sum = maths + english + science + urdu + sindhi
+document.writeln("your total marks out of 500 are :" +sum +"<br>")
+let percent = (sum/500*100)
+document.write('your percentage is :' + percent +"<br>")
+if(percent >=80){
+    document.write("A-1 grade")
+}
+else{
+    Document.write("your grade is F")
 }
